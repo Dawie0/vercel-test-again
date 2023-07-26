7191d3eb5ab57ec9250510e7f84b7c8425731226
Dawid Fourie <dawief4@gmail.com>
2023-07-26 15:58:50 -0700

    test 7

--- config/config.js
+++ config/config.js
@@ -1,9 +1,10 @@
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.DATABASE_URI, {
+        await mongoose.connect(process.env.MONGODB_URI, {
             useUnifiedTopology: true,
             useNewUrlParser: true
         });
+        console.log('connected to mongo')
     } catch (err) {
         console.error(err);
     }
